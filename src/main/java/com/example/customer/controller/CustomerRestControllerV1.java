44c135160c97c0b342ceadf2082db0bfa931914d
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-05-21 09:29:48 +0300

    add get all customers method

--- src/main/java/com/example/customer/controller/CustomerRestControllerV1.java
+++ src/main/java/com/example/customer/controller/CustomerRestControllerV1.java
@@ -2,29 +2,32 @@ package com.example.customer.controller;
 
 import com.example.customer.model.Customer;
 import com.example.customer.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/customers/")
 public class CustomerRestControllerV1 {
-    @Autowired
-    private CustomerService customerService;
+    private final CustomerService customerService;
+
+    public CustomerRestControllerV1(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     // TODO make GET MAPPING + produces deprecated
-    @RequestMapping(value = "id", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<Optional<Customer>> getCustomer(@PathVariable("id") Long customerId) {
         if (customerId == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         Optional<Customer> customer = customerService.findById(customerId);
 
-        if (customer == null) {
+        if (!customer.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
@@ -45,7 +48,7 @@ public class CustomerRestControllerV1 {
     }
 
     @RequestMapping(value = "", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<Customer> updateCustomer(Customer customer, UriComponentsBuilder builder) {
+    public ResponseEntity<Customer> updateCustomer(@RequestBody @Validated Customer customer, UriComponentsBuilder builder) {
         HttpHeaders httpHeaders = new HttpHeaders();
 
         if (customer == null) {
@@ -55,6 +58,32 @@ public class CustomerRestControllerV1 {
         customerService.save(customer);
 
         return new ResponseEntity<>(customer, httpHeaders, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Customer> deleteCustomer(@PathVariable("id") Long id) {
+        Optional<Customer> customer = customerService.findById(id);
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        if (customer == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        customerService.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+
+        List<Customer> customers = customerService.findAll();
+
+        if (customers == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(customers, HttpStatus.OK);
     }
 
 }
